9ce19d2e13acef5c67e2c5c6e9bf6695d1e0d009
prionz1 <Prionz@kkszki.hu>
2025-09-10 19:15:40 +0200

    chore: expand .gitignore, sort files in repo

---
+++ Desktop/BookStore/Data/Repositories/IRentalRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Data.Repositories
+{
+    internal interface IRentalRepository
+    {
+        void Read();
+        void Delete();
+    }
+}
