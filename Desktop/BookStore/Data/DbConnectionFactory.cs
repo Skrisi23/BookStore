bda01b92cb0891c77b34e1a260cc3dcc1e6b6511
prionz1 <Prionz@kkszki.hu>
2025-09-10 22:20:19 +0200

    feat: set up connection string
    - move conn string into ignored file
    - read the value of the file and assign it to _connstring variable

--- Desktop/BookStore/Data/DbConnectionFactory.cs
+++ Desktop/BookStore/Data/DbConnectionFactory.cs
@@ -1,6 +1,7 @@
 ﻿using MySqlConnector;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,18 +10,17 @@ namespace BookStore.Data
 {
     internal class DbConnectionFactory
     {
+        private readonly string _configPath = "Passwords/dbsettings.txt";
         private readonly string _connectionString;
-        
-        public DbConnectionFactory(string connectionString)
-        {
-            _connectionString = connectionString;      
-        }
 
+        public DbConnectionFactory()
+        {
+            _connectionString = File.ReadAllText(_configPath).Trim();      
+        }
+        
         public MySqlConnection CreateConnection()
         {
             return new MySqlConnection(_connectionString);
         }
-
-        
     }
 }
