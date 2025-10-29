b754407dec14d08ad9fdeeed318ac4edf8f351c8
prionz1 <prionz@kkszki.hu>
2025-10-29 22:08:06 +0100

    create models with dotnet scaffold command

---
+++ Backend/Backend/Domain/Model/BookStoreContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Domain.Model;
+
+public partial class BookStoreContext : DbContext
+{
+    public BookStoreContext(DbContextOptions<BookStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<author> authors { get; set; }
+
+    public virtual DbSet<book> books { get; set; }
+
+    public virtual DbSet<copy> copies { get; set; }
+
+    public virtual DbSet<rental> rentals { get; set; }
+
+    public virtual DbSet<user> users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("latin1_swedish_ci")
+            .HasCharSet("latin1");
+
+        modelBuilder.Entity<author>(entity =>
+        {
+            entity.HasKey(e => e.id).HasName("PRIMARY");
+        });
+
+        modelBuilder.Entity<book>(entity =>
+        {
+            entity.HasKey(e => e.id).HasName("PRIMARY");
+
+            entity.HasOne(d => d.author).WithMany(p => p.books)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_books_author");
+        });
+
+        modelBuilder.Entity<copy>(entity =>
+        {
+            entity.HasKey(e => e.id).HasName("PRIMARY");
+
+            entity.Property(e => e.elerheto).HasDefaultValueSql("'1'");
+
+            entity.HasOne(d => d.book).WithMany(p => p.copies)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_copies_book");
+        });
+
+        modelBuilder.Entity<rental>(entity =>
+        {
+            entity.HasKey(e => e.id).HasName("PRIMARY");
+
+            entity.HasOne(d => d.copy).WithMany(p => p.rentals)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_rentals_copy");
+
+            entity.HasOne(d => d.user).WithMany(p => p.rentals)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("fk_rentals_user");
+        });
+
+        modelBuilder.Entity<user>(entity =>
+        {
+            entity.HasKey(e => e.id).HasName("PRIMARY");
+
+            entity.Property(e => e.letrehozva).HasDefaultValueSql("current_timestamp()");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Backend/Backend/Domain/Model/book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Domain.Model;
+
+[Index("author_id", Name = "author_id")]
+[MySqlCharSet("utf8mb4")]
+[MySqlCollation("utf8mb4_hungarian_ci")]
+public partial class book
+{
+    [Key]
+    [Column(TypeName = "int(11)")]
+    public int id { get; set; }
+
+    [StringLength(255)]
+    public string cim { get; set; } = null!;
+
+    [Column(TypeName = "int(11)")]
+    public int author_id { get; set; }
+
+    public DateOnly? kiadasi_datum { get; set; }
+
+    [Column(TypeName = "text")]
+    public string? tartalom { get; set; }
+
+    [ForeignKey("author_id")]
+    [InverseProperty("books")]
+    public virtual author author { get; set; } = null!;
+
+    [InverseProperty("book")]
+    public virtual ICollection<copy> copies { get; set; } = new List<copy>();
+}
