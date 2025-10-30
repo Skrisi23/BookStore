0c31f4bbe5f71c026d67b0613495bdf41ffe889c
prionz11 <prionz@kkszki.hu>
2025-10-30 18:40:29 +0100

    create controller for book model and add cors policy

---
+++ Backend/Backend/Api/Controllers/BooksController.cs
@@ -0,0 +1,66 @@
+﻿using Backend.Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Backend.Api.Controllers
+{
+    [ApiController]
+    [Route("Api/[controller]")]
+
+    public class BooksController : Controller
+    {
+
+        private readonly BookStoreContext _context;
+
+        public BooksController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            return Ok(_context.books.ToList());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(ulong id)
+        {
+            var user = _context.books.Find(id);
+            return Ok(user);
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(book book)
+        {
+            _context.books.Add(book);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new {id = book.id }, book);
+        }
+        
+
+        [HttpPut]
+        public IActionResult Update(ulong id, book book)
+        {
+            _context.Entry(book).State = EntityState.Modified;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
+
+        [HttpDelete]
+        public IActionResult DeleteById(ulong id)
+        {
+            var book = _context.books.Find(id);
+            _context.Remove(book);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+
+    }
+}
