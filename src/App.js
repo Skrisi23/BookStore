30f24f8a81eebe17cb4258f9edf3e4c11287e5a9
ezjolesz <toth.kende06@gmail.com>
2025-11-05 17:24:05 +0100

    feat: integrate all pages in App.js with routing logic

--- src/App.js
+++ src/App.js
@@ -1,71 +1,54 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
+// src/App.js
+import React, { useState } from 'react';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer';
+import HomePage from './pages/HomePage';
 import BooksPage from './pages/BooksPage';
-import AuthorsPage from './pages/AuthorsPage';
 import AboutPage from './pages/AboutPage';
-import * as api from './services/api';
+import LoginPage from './pages/LoginPage';
+import CartPage from './pages/CartPage';
+import AdminPage from './pages/AdminPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('books');
-  const [books, setBooks] = useState([]);
-  const [authors, setAuthors] = useState([]);
-
-  useEffect(() => {
-    loadData();
-  }, []);
-
-  const loadData = async () => {
-    try {
-      const [booksData, authorsData] = await Promise.all([
-        api.fetchBooks(),
-        api.fetchAuthors()
-      ]);
-      setBooks(booksData);
-      setAuthors(authorsData);
-    } catch (error) {
-      console.error('Error loading data:', error);
-      alert('Hiba történt az adatok betöltése során');
-    }
-  };
+  const [currentPage, setCurrentPage] = useState('home');
 
   const renderPage = () => {
     switch (currentPage) {
+      case 'home':
+        return <HomePage onNavigate={setCurrentPage} />;
       case 'books':
-        return (
-          <BooksPage
-            books={books}
-            authors={authors}
-            onAddBook={api.addBook}
-            onUpdateBook={api.updateBook}
-            onDeleteBook={api.deleteBook}
-            onRefresh={loadData}
-          />
-        );
-      case 'authors':
-        return (
-          <AuthorsPage
-            authors={authors}
-            onAddAuthor={api.addAuthor}
-            onUpdateAuthor={api.updateAuthor}
-            onDeleteAuthor={api.deleteAuthor}
-            onRefresh={loadData}
-          />
-        );
+        return <BooksPage />;
       case 'about':
         return <AboutPage />;
+      case 'login':
+        return <LoginPage onLoginSuccess={() => setCurrentPage('home')} />;
+      case 'cart':
+        return <CartPage onNavigate={setCurrentPage} />;
+      case 'admin':
+        return <AdminPage onNavigate={setCurrentPage} />;
       default:
-        return <BooksPage books={books} authors={authors} onRefresh={loadData} />;
+        return <HomePage onNavigate={setCurrentPage} />;
     }
   };
 
   return (
-    <div className="App">
-      <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
-      <div className="container mt-4">
-        {renderPage()}
-      </div>
-    </div>
+    <AuthProvider>
+      <CartProvider>
+        <div className="App d-flex flex-column min-vh-100">
+          <Navbar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+          <main className="flex-grow-1">
+            {renderPage()}
+          </main>
+          <Footer />
+        </div>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
