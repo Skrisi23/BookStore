a6f94e903c128f384c6334390d21efd21acd07f7
ezjolesz <toth.kende06@gmail.com>
2026-02-24 03:19:04 +0100

    fix: Display sequential row numbers instead of database IDs in admin book list

--- src/components/admin/BookManagement.js
+++ src/components/admin/BookManagement.js
@@ -195,9 +195,9 @@ function BookManagement() {
               </tr>
             </thead>
             <tbody>
-              {books.map(book => (
+              {books.map((book, index) => (
                 <tr key={book.id}>
-                  <td>#{book.id}</td>
+                  <td>#{index + 1}</td>
                   <td>{book.title}</td>
                   <td>{book.author}</td>
                   <td>
